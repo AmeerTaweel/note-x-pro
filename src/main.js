1fd59037ad0815bb994207d39865cbdcce6c84d0
Ameer Taweel <ameertaweel2002@gmail.com>
2018-08-24 18:59:30 +0300

    Create and setup the vuex store.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Notifications from 'vue-notification'
+import Store from './store/store.js'
 
 new Vue({
   el: '#app',
+  store: Store,
   render: h => h(App)
 })
 
