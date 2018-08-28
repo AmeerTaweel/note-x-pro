0026f240e465349703f81d18229c44d556d5dbac
Ameer Taweel <ameertaweel2002@gmail.com>
2018-08-28 17:00:19 +0300

    Install and configure vue-router.

--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
 import Vue from 'vue'
-import App from './App.vue'
-import Notifications from 'vue-notification'
+import VueRouter from 'vue-router'
 import Store from './store/store.js'
+import Notifications from 'vue-notification'
+import App from './App.vue'
+import Editor from './components/Editor.vue'
+
+const router = new VueRouter({
+  routes: [
+    {path: '', Editor}
+    ]
+})
 
 new Vue({
   el: '#app',
   store: Store,
+  router: router,
   render: h => h(App)
 })
 
