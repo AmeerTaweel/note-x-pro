4ad3cf94b11756a61a77571adfbf358707e882d1
Ameer Taweel <ameertaweel2002@gmail.com>
2018-08-29 16:46:57 +0300

    App Component: Polish title.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app" class="m-20">
     <h1 class="text-center">Note<span class="text-primary">X</span>Pro</h1>
-    <h1 class="text-center"><small>Fast, Clean, Responsive &#38; Professional Notepad!</small></h1>
+    <h1 class="text-center"><small><span class="text-primary">F</span>ast, <span class="text-primary">C</span>lean, <span class="text-primary">R</span>esponsive &#38; <span class="text-primary">P</span>rofessional <span class="text-primary">N</span>otepad!</small></h1>
     <router-view></router-view>
   </div>
 </template>
